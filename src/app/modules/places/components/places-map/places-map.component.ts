81cb6e404259eadb1f3901fceb523e03375c5499
operta <dzanoperta@yahoo.com>
2022-01-23 21:49:26 +0100

    create map markers from loaded place locations

--- src/app/modules/places/components/places-map/places-map.component.ts
+++ src/app/modules/places/components/places-map/places-map.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {catchError, map, Observable, of} from "rxjs";
+import {PlaceLocation} from "../../models/place-location.model";
 
 @Component({
   selector: 'app-places-map',
@@ -9,6 +10,7 @@ import {catchError, map, Observable, of} from "rxjs";
 })
 export class PlacesMapComponent implements OnInit {
   apiLoaded$: Observable<any>;
+  placeLocationMarkers$: Observable<google.maps.LatLng[]> | undefined;
 
   constructor(httpClient: HttpClient) {
     this.apiLoaded$ = httpClient.jsonp("https://maps.googleapis.com/maps/api/js?key=AIzaSyBm3JBOsD5b6m4LbE1Yn0FP8aIIo6jT2Og", "callback")
@@ -19,7 +21,27 @@ export class PlacesMapComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.placeLocationMarkers$ = this.loadPlaces()
+      .pipe(
+        map((res: PlaceLocation[]) =>
+          this.convertPlaceLocationsToMarkers(res)
+        )
+      )
+  }
 
+  private convertPlaceLocationsToMarkers(placeLocations: PlaceLocation[]) {
+    return placeLocations.flatMap(
+      (location) => new google.maps.LatLng(location.latitude, location.longitude)
+    );
+  }
+
+  private loadPlaces(): Observable<PlaceLocation[]> {
+    return of(
+      [
+        new PlaceLocation(1, 37.423548, -122.084819),
+        new PlaceLocation(2, 37.419202, -122.086503)
+      ]
+    );
   }
 
 }
