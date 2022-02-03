378547ad731c5a6ce3cf255d3556d7f5b5b35977
operta <dzanoperta@yahoo.com>
2022-02-03 12:23:41 +0100

    init rxjs spy to track observables

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import {create} from "rxjs-spy";
 
 if (environment.production) {
   enableProdMode();
+} else {
+  //we enable RXjs Spy on non production bulds only
+  const spy = create();
+  // we call show for two purposes: first is to log to the console an empty snapshot so we can see that everything is working as expected, then to suppress unused variable usage (the latter is a convention on mine)
+  spy.show();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
